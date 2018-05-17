9307ef0e4f6df34afa52f4856720797dce5aa7f8
Aaron Villanueva <aarone.villanueva@gmail.com>
2018-05-17 14:06:16 -0700

    fixes files and folders

--- src/app.js
+++ src/app.js
@@ -8,23 +8,15 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-// app.use('/', require('./routes'))
-
-// app.use((error, req, res, next) => {
-//   res.status(500).render('./errors/error', {error})
-// })
-
-// using google token to see if user is logged in or not
-
 app.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.send('index.html')
 })
 
 app.use((req, res) => {
   res.send('404 NOT FOUND')
 })
 
-const port = process.env.PORT || 3002
+const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}...`)
 })
